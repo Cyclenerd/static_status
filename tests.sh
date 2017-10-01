9a18088d4413a4e3dc7f867bbb55394b9ca9f4c1
Nils <git@velohero.com>
2017-10-01 12:43:01 +0200

    freenet server removed

--- tests.sh
+++ tests.sh
@@ -6,9 +6,9 @@
 
 mkdir "$HOME/status" &> /dev/null
 cat > "$HOME/status/status_hostname_list.txt" << EOF
-ping;ftp.freenet.de
-nc;ftp.freenet.de;21
-curl;http://ftp.freenet.de/README
+ping;www.heise.de
+nc;www.heise.de;80
+curl;https://www.heise.de/ping
 ping;gibt.es.nicht.nkn-it.de
 nc;gibt.es.nicht.nkn-it.de;21
 curl;http://gibt.es.nicht.nkn-it.de
@@ -33,16 +33,16 @@ assert "bash status.sh silent"
 
 # $ bash status.sh loud
 #
-# UP:   ping ftp.freenet.de
-# UP:   nc   ftp.freenet.de FTP
-# UP:   curl http://ftp.freenet.de/README
+# UP:   ping www.heise.de
+# UP:   nc   www.heise.de HTTP
+# UP:   curl https://www.heise.de/ping
 # DOWN: ping gibt.es.nicht.nkn-it.de
 # DOWN: nc   gibt.es.nicht.nkn-it.de FTP
 # DOWN: curl http://gibt.es.nicht.nkn-it.de
 # DOWN: curl https://www.nkn-it.de/404
 assert_raises "bash status.sh loud"
 
-assert "cat $HOME/status/status_hostname_ok.txt | grep 'ping;ftp.freenet.de'" "ping;ftp.freenet.de;"
-assert "cat $HOME/status/status_hostname_ok.txt | grep 'http://ftp.freenet.de/README'" "curl;http://ftp.freenet.de/README;"
+assert "cat $HOME/status/status_hostname_ok.txt | grep 'ping;www.heise.de'" "ping;www.heise.de;"
+assert "cat $HOME/status/status_hostname_ok.txt | grep 'https://www.heise.de/ping'" "curl;https://www.heise.de/ping;"
 assert "cat $HOME/status/status_hostname_down.txt | grep 'nkn-it.de' | wc -l | perl -pe 's/\s//g'" "4"
 assert_end status_sh
